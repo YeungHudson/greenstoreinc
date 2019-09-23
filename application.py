25ebcaf22c2f0ecf2020aaa92cc92ce82286133f
Hudson Yeung <hudsonyeung2000@gmail.com>
2019-09-23 12:45:00 -0400

    added basic foundation

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+@app.route("/")
+def index:
+    return render_template("index.html")
